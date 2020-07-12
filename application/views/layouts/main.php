9afbfa9a461e472623bd19badc2f08cca305e995
alejandro <ale.hv@gmail.com>
2020-07-12 11:22:42 -0400

    logos

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -98,7 +98,7 @@
             <aside class="main-sidebar sidebar-dark-primary elevation-4">
                 <!-- Brand Logo -->
                 <a href="#" class="brand-link">
-                  <img src="<?php echo site_url('resources/img/user2-160x160.jpg');?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+                  <img src="<?php echo site_url('resources/images/logo2.png');?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                        style="opacity: .8">
                   <span class="brand-text font-weight-light"><b>BUSINESS</b> SYS</span>
                 </a>
